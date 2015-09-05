f19583d265c9ef21b024b19bdc94cbe10360b51c
matthaldeman95 <matthaldeman95@gmail.com>
2015-09-05 14:42:02 -0400

    Fixed DCUNext game python codes

--- GUIAttempt1/shittyfuckingbullshitv2.py
+++ GUIAttempt1/shittyfuckingbullshitv2.py
@@ -2,7 +2,7 @@ import wx
 import wx.lib.newevent
 import serial
 import time
-ser = serial.Serial(port ='/dev/cu.usbmodemfd121',baudrate=115200)
+ser = serial.Serial(port ='COM4',baudrate=115200)
 print(ser.isOpen())
 
 class Window(wx.Frame):
@@ -27,9 +27,10 @@ class Window(wx.Frame):
         wx.TextCtrl(panel, pos=(10,10),size=(200,200))
         ledon = wx.Button(panel, label = "LED On", pos = (310,20), size = (100,20))
         ledoff = wx.Button(panel, label = "LED Off", pos=(310,40), size = (100,20))
+        flash = wx.Button(panel, label = "Flash", pos=(310,60),size=(100,20))
         panel.Bind(wx.EVT_BUTTON, self.LEDON, ledon)
         panel.Bind(wx.EVT_BUTTON, self.LEDOFF, ledoff)
-
+        panel.Bind(wx.EVT_BUTTON, self.FLASHfn, flash)
 
     def Quit(self, e):
         self.Close()
@@ -38,6 +39,13 @@ class Window(wx.Frame):
         ser.write("ON")
     def LEDOFF(self, e):
         ser.write("OFF")
+    def FLASHfn(self, e):
+        for x in range (1,6):
+            ser.write("ON")
+            time.sleep(1)
+            ser.write("OFF")
+            time.sleep(1)
+
 
 
 
