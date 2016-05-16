c73c50610b68defcc8c8ce1e088dcf1546a4bfea
Matthew Haldeman <haldeman3@marshall.edu>
2016-05-16 10:52:30 -0400

    Cleaning up folders and removing old stuff

--- PycharmProjects/YunCodesSuccessful/getcond.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-import BeautifulSoup
-import newurllib2
-Soup = BeautifulSoup.BeautifulSoup
-response = newurllib2.urlopen('http://weather.yahooapis.com/forecastrss?w=2425834').read()
-soup = Soup(response)
-cond = soup.find("yweather:condition")['text']
-print cond
--- untitled1/Problem 17.4.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-from matplotlib import pyplot as plt
-x = (0:100:100)
-plt.plot(np.sin(x))
