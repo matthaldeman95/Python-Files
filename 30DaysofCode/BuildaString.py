67baabd00df76c9153df68939b11e291659f47b8
Matthew Haldeman <matthaldeman95@gmail.com>
2016-08-14 17:45:39 -0400

    Added folder for 30 days of code challenge

---
+++ 30DaysofCode/BuildaString.py
@@ -0,0 +1,40 @@
+
+
+def buildaString(inString, A, B):
+    outString = ""
+    cost = 0
+    letter = 0
+    while letter <= len(inString)-1:
+        foundin = False
+        for rightind in range(len(inString), letter+1, -1):
+            subString = inString[letter:rightind]
+            if subString in outString:
+                outString += subString
+                cost += B
+                foundin = True
+                letter += len(subString)
+                break
+        if not foundin:
+            outString += inString[letter]
+            cost += A
+            letter += 1
+    return cost
+
+A = []
+B = []
+inputString = []
+
+
+testcases = raw_input("Enter number of test cases:  ")
+for n in range(0,int(testcases)):
+    digits = raw_input("Enter length of string, cost A, and cost B, "
+                       "separated by spaces:  ")
+    N, a, b = digits.split(" ")
+    A.append(int(a))
+    B.append(int(b))
+    inputString.append(raw_input("Type the string to build:  "))
+
+for n in range(0,len(A)):
+    print buildaString(inputString[n], A[n], B[n])
+
+
